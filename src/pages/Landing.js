fd166c16b485601600a881bcd0b6699182938c3a
NotSmurf1245 <trace.davis2020@outlook.com>
2023-02-01 17:53:10 -0600

    clean up

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -8,8 +8,8 @@ function Landing() {
       <div>
         <ImageCarrosel />
         <h1 className="bodyTitle">About Us</h1>
-        <ImageRightBO title="Title" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Purus faucibus ornare suspendisse sed. Diam maecenas sed enim ut sem viverra aliquet. Ultrices eros in cursus turpis massa tincidunt dui ut. Faucibus in ornare quam viverra orci. Arcu cursus vitae congue mauris rhoncus aenean vel."/>
-        <ImageLeftBO title="Title" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Purus faucibus ornare suspendisse sed. Diam maecenas sed enim ut sem viverra aliquet. Ultrices eros in cursus turpis massa tincidunt dui ut. Faucibus in ornare quam viverra orci. Arcu cursus vitae congue mauris rhoncus aenean vel."/>
+        <ImageRightBO title="History" body=""/>
+        <ImageLeftBO title="Title" body=""/>
       </div>
 
   )
